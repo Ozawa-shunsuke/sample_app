d4362cef587db0ee2fbf7a073a8aa960e30c8c9f
ozawa shunsuke <ozawa.shunsuke@viva-eureka.co.jp>
2020-06-02 08:07:28 +0000

    heroku befoe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://immense-chamber-14378.herokuapp.com.herokuapp.com'
+  host = 'https://immense-chamber-14378.herokuapp.com/.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.amtp_settings = {
     :address        => 'smtp.sendgrid.net',
