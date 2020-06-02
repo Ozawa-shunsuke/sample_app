2425f06fcaa57c1457b519df55ba1dbc9f522340
ozawa shunsuke <ozawa.shunsuke@viva-eureka.co.jp>
2020-06-02 08:14:46 +0000

    third commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,15 +92,15 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://immense-chamber-14378.herokuapp.com/.herokuapp.com'
+  host = 'https://immense-chamber-14378.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.amtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => EVN['SENDGRID_PASSWORD'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-  }  
+  }
 end
